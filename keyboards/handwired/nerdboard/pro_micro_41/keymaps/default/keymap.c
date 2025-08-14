7b3bbe268c22fc4397cc13c0b560a6c35539bae7
thelowprokill <jonathan.hull@beefnw.com>
2025-08-14 07:28:10 -0700

    adjust-pappy-layer

--- keyboards/handwired/nerdboard/pro_micro_41/keymaps/default/keymap.c
+++ keyboards/handwired/nerdboard/pro_micro_41/keymaps/default/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,       KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSPC,
     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,       KC_H   , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_RSFT,
     KC_LALT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,       KC_N   , KC_M  ,KC_COMM, KC_DOT,KC_SLSH,KC_LCTL,
-                            KC_ENT, MO_TAB, KC_SPC ,       SY_GRV ,_______
+                             SYMBOL, MO_TAB, KC_SPC ,      SY_GRV , KC_ENT
   ),
   [_SYMBOL] = LAYOUT(
     _______,_______, KC_7  , KC_8  , KC_9  ,KC_LCBR,       KC_RCBR,KC_MINS, PLUS  , STAR  ,KC_SLSH,_______,
